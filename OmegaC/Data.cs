1aa9454b1ca6f24e2a6349902e63c9be1de565ae
Younggil Lee <leyoungg@sheridancollege.ca>
2017-04-02 19:29:45 -0400

    Create Data class for connecting DB and setting connection string in web.config

---
+++ OmegaC/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OmegaC.Data))]
+
+namespace OmegaC
+{
+    public class Data
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
