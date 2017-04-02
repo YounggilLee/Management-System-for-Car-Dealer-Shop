697948674add4f91fc0f4b552ab16163de57d4cb
Younggil Lee <leyoungg@sheridancollege.ca>
2017-04-02 19:25:03 -0400

    Create project and data tables input data

---
+++ OmegaWind.sql
@@ -0,0 +1,99 @@
+﻿-- File Name:	 Creat Tab Main.sql
+-- Student Name: Younggil Lee (991 395 505)
+-- Description:	 Create Tables for c#
+
+-- 1. drop tables if they exist
+DROP TABLE options;
+DROP TABLE service;
+DROP TABLE invoice;
+DROP TABLE employee;
+DROP TABLE car;
+DROP TABLE customer;
+
+-- 2. create tables
+CREATE TABLE customer
+(
+ customerID 	DECIMAL(10),
+ firstName		CHAR(10),
+ lastName		CHAR(10),
+ address		CHAR(50),
+ phone			DECIMAL(10),
+ CONSTRAINT pk_customer PRIMARY KEY (customerID) 
+ );
+
+CREATE TABLE car
+(
+ serial CHAR(10), 
+ make CHAR(10),
+ model CHAR(8),
+ cyear	DECIMAL(5),
+ price	DECIMAL(12),
+ CONSTRAINT pk_car PRIMARY KEY (serial)
+ );
+  
+ 
+ CREATE TABLE employee
+(
+ employeeID DECIMAL(10),
+ password	CHAR(10),
+ firstName	CHAR(10),
+ lastName	CHAR(10),
+ salary		DECIMAL(10),
+ sales		DECIMAL(10),
+ commission	DECIMAL(10),
+ CONSTRAINT pk_employee PRIMARY KEY (employeeID) 
+ );
+ 
+  CREATE TABLE invoice
+(
+ invNum		DECIMAL(10),
+ invDate	DATE,
+ netPrice	DECIMAL(10),
+ tax		DECIMAL(10),
+ otherFees	DECIMAL(10),
+ customerID DECIMAL(10),
+ serial     CHAR(10),
+ employeeID DECIMAL(10), 
+ CONSTRAINT pk_invoice PRIMARY KEY (invNum),
+ CONSTRAINT fk1_invoice FOREIGN KEY (customerID) REFERENCES customer (customerID),
+ CONSTRAINT fk2_invoice FOREIGN KEY (serial) REFERENCES car (serial),
+ CONSTRAINT fk3_invoice FOREIGN KEY (employeeID) REFERENCES employee (employeeID)
+ );
+ 
+   CREATE TABLE service
+(
+ servNum	DECIMAL(10),
+ laborPrice	DECIMAL(10),
+ partPrice	DECIMAL(10),
+ tax		DECIMAL(10),
+ info	 	CHAR(200),
+ customerID DECIMAL(10),
+ serial 	CHAR(10), 
+ CONSTRAINT pk_service PRIMARY KEY (servNum),
+ CONSTRAINT fk1_service FOREIGN KEY (customerID) REFERENCES customer (customerID),
+ CONSTRAINT fk2_service FOREIGN KEY (serial) REFERENCES car (serial) 
+ );
+ 
+    CREATE TABLE options
+(
+ optionCode	CHAR(10),
+ optionPrice DECIMAL(10),
+ OptionDesc	CHAR(100),
+ serial 	CHAR(10), 
+ CONSTRAINT pk_options PRIMARY KEY (optionCode),
+ CONSTRAINT fk1_options FOREIGN KEY (serial) REFERENCES car (serial) 
+ );
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ OmegaWindDataInsert.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO customer(customerID, firstName, lastName, address, phone)
+VALUES(1,'Younggil', 'lee', 'Oakvile',1111111111 ),(2,'Choongwon', 'lee', 'Missisauga',2222222222);
+
+INSERT INTO car(serial, make, model, cyear, price)
+VALUES('A1','AQURA', 'MDX', 2017, 10000 ),('A2','BMW', 'M3', 2016 ,20000);
+
+INSERT INTO employee(employeeID, password, firstName, lastName, salary, sales, commission)
+VALUES(1,'1234', 'yg', 'lee', 10000, 20000, 300),(2,'1234', 'ch', 'lee', 40000, 50000, 200);
+
+INSERT INTO invoice(invNum, invDate, netPrice, tax, otherFees, customerID, serial,employeeID )
+VALUES(1,'2016-07-21', 40000, 400, 100, 1, 'A1', 1),(2,'2016-05-21', 30000, 300, 100, 2, 'A2', 2);
+
+INSERT INTO service(servNum, laborPrice, partPrice, tax, info, customerID, serial)
+VALUES(1, 300, 400, 40, 'chage window', 1, 'A1'),(2, 300, 400, 30, 'add lamp', 2, 'A2');
+
+INSERT INTO options(optionCode, optionPrice, OptionDesc, serial)
+VALUES('o1', 300,'front window','A1'),('o2', 400,'side window','A2');
